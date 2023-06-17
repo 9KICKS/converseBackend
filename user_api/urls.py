292759ce2fa2389c2c8016cc175066af82ef0d6a
9KICKS <officialjoshua9@gmail.com>
2023-06-17 16:31:53 +0100

    "updated the app"

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('register', views.UserRegister.as_view(), name='register'),
     path('login', views.UserLogin.as_view(), name='login'),
     path('user', views.UserView.as_view(), name='user'),
-
-    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
-    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
+    path('reset', views.CustomPasswordResetView.as_view(), name='reset'),
+    path('change/password', views.CustomPasswordChangeView.as_view(), name='change password')
 ]
